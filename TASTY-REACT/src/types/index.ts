c7c93eab3e546dc2e560c3fdf55e0f98f2ab6dd9
sathvikguntukagithub <sathvik.guntuka@gmail.com>
2024-05-11 16:07:20 +0530

    Add beginner react project

---
+++ TASTY-REACT/src/types/index.ts
@@ -0,0 +1,19 @@
+export interface Recipe {
+  id: number;
+  name: string;
+  cuisine: string;
+  prepTimeMinutes: number;
+  cookTimeMinutes: number;
+  servings: number;
+  image: string;
+  rating: number;
+}
+
+export interface Data {
+  recipes: Recipe[];
+}
+export type RecipeFetchState = {
+  data: Recipe | null;
+  loading: boolean;
+  error: Error | null;
+};
